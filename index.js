6f703e109c1305070aa49829374e807a3b63e564
unknown <potokopi123456@gmail.comgit config --global user.email potokopi123456@gmail.com>
2022-05-31 19:29:09 +0700

    netflixHD

---
+++ index.js
@@ -0,0 +1,26 @@
+const headers = document.querySelectorAll(".faq-header")
+
+headers.forEach(header => {
+    let hidden = true
+    header.addEventListener("click", () => {
+        if(hidden) {
+            headers.forEach(head => {
+                head.nextSibling.nextSibling.classList.add("hidden")
+                head.children[1].innerHTML = "&#10011;"
+            })
+            header.nextSibling.nextSibling.classList.remove("hidden")
+            header.children[1].innerHTML = "&#10005;"
+            hidden = false
+        } else {
+            header.nextSibling.nextSibling.classList.add("hidden")
+            header.children[1].innerHTML = "&#10011;"
+            hidden = true
+        }
+    })
+})
+
+
+function gotoOffer() {
+    // ganti link dengan link offer
+    window.location.href = "https://www.google.com";
+}
